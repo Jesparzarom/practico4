2248117418d90837e6b4be04227b0ba3faee06c3
Juan Esparza R <jesparzarom@outlook.com>
2023-03-20 04:12:50 +0000

    Add files and more

---
+++ js/01-04_alerta.js
@@ -0,0 +1,13 @@
+/* === EJERCICIO 1 Y 2 ===
+alert("Esto es JavaScript");
+*/
+
+/* === EJERCICIO 3 ===
+document.write("Texto");
+*/
+
+/* === EJERCICIO 4 ===*/
+var Edad= "Tengo 14 años";
+Edad = "Ahora Tengo 15 años";
+document.write (Edad);
+
